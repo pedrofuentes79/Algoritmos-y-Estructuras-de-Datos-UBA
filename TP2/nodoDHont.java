6bb3260b7b915a565e94444cf2cdc0a14e545235
Pedro Fuentes <66275090+pedrofuentes79@users.noreply.github.com>
2023-11-03 20:18:24 -0300

    tp2

---
+++ TP2/nodoDHont.java
@@ -0,0 +1,15 @@
+package aed;
+
+public class nodoDHont{
+    private int idPartido;
+    private int cocientes;
+    private int vecesDividido;
+
+    public nodoDHont(int idPartido, int cocientes){
+        this.idPartido = idPartido;
+        this.cocientes = cocientes;
+        this.vecesDividido = 1;
+    }
+
+}
+
